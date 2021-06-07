eb03362b0e784af099581145ffc9048e37c1b6b5
Nitigya Jain <nitigyajain123456@gmail.com>
2021-06-07 18:57:35 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AutoAdmit
+Chrome Extension to Automatically admit participants in a google meet call
+
---
+++ content.js
@@ -0,0 +1,8 @@
+function admit() 
+{
+    var admitBtn = document.getElementsByClassName('CwaK9')[1];
+    admitBtn.click();
+}
+
+setInterval(admit, 1000)
+
